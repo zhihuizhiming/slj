9b8afc423f48fc2872653ffdf1a2be6091add202
root <root@host.localdomain>
2017-11-30 06:48:37 -0500

    修复psql创建数据库脚本错误

--- postgres/docker-entrypoint-initdb.d/slj_web.sh
+++ postgres/docker-entrypoint-initdb.d/slj_web.sh
@@ -1,3 +1,3 @@
-#!/bin/env bash
-psql -U postgres -c "CREATE USER  PASSWORD ''"
-psql -U postgres -c "CREATE DATABASE  OWNER "
+#!/bin/bash
+psql -U postgres -c "CREATE USER $DB_USER PASSWORD '$DB_PASS'"
+psql -U postgres -c "CREATE DATABASE $DB_NAME OWNER $DB_USER"
